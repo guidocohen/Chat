ab964d53f8eac90292901019bc79c3e2a7ef5e55
Guido <guidocohens@gmail.com>
2020-06-01 01:18:15 -0300

    Diseño interfaces login

---
+++ app/src/main/java/com/guido/chat/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.guido.chat.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.guido.chat.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
