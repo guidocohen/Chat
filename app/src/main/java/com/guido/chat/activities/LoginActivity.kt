cde851f9f6038ddd5f86ef2812fefebc45000c1f
Guido <guidocohens@gmail.com>
2020-06-03 22:08:21 -0300

    Firebase Auth Ok

--- app/src/main/java/com/guido/chat/activities/LoginActivity.kt
+++ app/src/main/java/com/guido/chat/activities/LoginActivity.kt
@@ -1,14 +1,25 @@
 package com.guido.chat.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.guido.chat.*
 import kotlinx.android.synthetic.main.activity_login.*
 
+
 class LoginActivity : AppCompatActivity() {
 
-    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +42,10 @@ class LoginActivity : AppCompatActivity() {
             overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
         }
 
+        buttonLogInGoogle.setOnClickListener {
+            signIn()
+        }
+
         editTextEmail.validate {
             editTextEmail.error = if (isValidEmail(it)) null else "Email is not valid."
         }
@@ -43,11 +58,54 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun logInByEmail(email: String, password: String) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
             if (it.isSuccessful) {
-                if (mAuth.currentUser!!.isEmailVerified) toast("Your user is now logged in.")
+                if (auth.currentUser!!.isEmailVerified) toast("Your user is now logged in.")
                 else toast("Must confirm email first.")
             } else toast("Invalid email or password.")
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                Snackbar.make(loginLayout, "Authentication Failed", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun signIn() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val user = auth.currentUser
+                    Snackbar.make(loginLayout, "Authentication success.", Snackbar.LENGTH_SHORT)
+                        .show()
+
+                    //updateUI(user)
+                } else {
+                    Snackbar.make(loginLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT)
+                        .show()
+                    //updateUI(null)
+                }
+            }
     }
 }
